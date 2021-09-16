284304d65702a69221b551adb104adb8574d4cd6
Pantiukhov Roman <pantiukhov.r@gmail.com>
2021-09-17 01:42:26 +0300

    NewCommitNewBranch2

--- src/TestGit.java
+++ src/TestGit.java
@@ -1,7 +1,8 @@
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("NewBranch1");
+        System.out.println();
         System.out.println("WTF");
+        System.out.println("NewBranch2");
     }
 }
