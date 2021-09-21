92addbbee51f3777d50a1b313d9472c67a686c68
Pantiukhov Roman <pantiukhov.r@gmail.com>
2021-09-21 23:28:34 +0300

    New Branch COMMIT new 11

--- src/Car/Transport.java
+++ src/Car/Transport.java
@@ -3,6 +3,7 @@ package Car;
 public class Transport {
     public static void main(String[] args) {
         System.out.println("new 1");
+        System.out.println();
     }
     String type = "Non";
 }
